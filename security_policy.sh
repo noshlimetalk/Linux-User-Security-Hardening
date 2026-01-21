3373651a3a7b58bb2b5648ba4216ec889fab539d
Krisney Manuel <krisney83@gmail.com>
2026-01-21 16:40:25 -0500

    Add script to enforce password policies for users
    This script applies password policies to a specified user, including expiration and minimum change periods, and updates the password quality configuration.

--- security_policy.sh
+++ security_policy.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+LOG="/var/log/user_admin.log"
 
+echo "Enter username to apply password policies:"
+read USERNAME
+
+# Password aginf for user accounts
+sudo chage -M 90 $USERNAME # User password expires in 90 days
+sudo chage -m 7 $USERNAME # Minimum 7 days between being able to change password
+sudo chage -W 7 $USERNAME #Warns user 7 days before password expiry
+
+# Strong password policy (pwquality)
+
+sudo cp config/pwquality.conf /etc/security/pwquality.conf
+
+echo "$(date) - Password policies applies to $USERNAME" | sudo tee -s $LOG
+echo "Password security policies applied."
