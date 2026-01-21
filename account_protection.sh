2c29e6c9fd1b4cae2ebc407802bb4f9d09c3d05f
Krisney Manuel <krisney83@gmail.com>
2026-01-21 16:47:49 -0500

    Update account_protection.sh

--- account_protection.sh
+++ account_protection.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+LOG="/var/log/user_admin.log"
+echo "Configuring account lockout policy (Ubuntu)"
+
+sudo bash -c 'cat >> /etc/pam.d/common-auth << EOF auth required pam.tally2.so deny=3 unlock_time=900 onerrr=fail audit EOF'
+
+sudo bash -c 'cat >> /etc/pam.d/common-account << EOF account required pam_tally2.so EOF'
+
+echo "$(date) - Account lockout policy enabled (3 attemps /15 min)" | sudo tee -a $LOG
+echo "Account protection policy applied"
 
