63a048fae823acd27bf655048d078a3de28d4038
Krisney Manuel <krisney83@gmail.com>
2026-01-21 17:11:05 -0500

    Add interactive menu for user administration

--- admin_tool.sh
+++ admin_tool.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+while true; do
+echo ""
+echo "======= Linux User Admin Tool ======"
+echo "1 - onboar user"
+echo "2- Apply Password security"
+echo "3- Apply account protection"
+echo "4 - Offboard user"
+echo "5 - exit"
+echo "========================================"
+
+read OPTION
+
+case $OPTION in
+1) sudo ./onboarding.sh ;;
+2) sudo ./security_policy.sh ;;
+3) sudo ./account_protectin.sh ;;
+4) sudo ./offboarding.sh ;;
+5) exit 0 ;;
+*) echo "Invalid option" ;;
+esac
+done
