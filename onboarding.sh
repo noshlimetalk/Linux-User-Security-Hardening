4f8bfd464f72c3fe90a34085d57c7ee96dc9dfce
Krisney Manuel <krisney83@gmail.com>
2026-01-21 17:34:02 -0500

    Add onboarding script for new users
    Script to onboard a new user with department assignment and log the action.

--- onboarding.sh
+++ onboarding.sh
@@ -1,1 +1,21 @@
-on
+#!/bin/bash
+
+LOG="/var/log/user_admin.log"
+
+echo "Enter new username"
+read USERNAME
+
+echo "Enter department:"
+read DEPT
+
+sudo useradd -m $USERNAME
+echo "$USERNAME:$TEMP_PASS" |sudo chpasswd
+sudo usermod -aG $DEPT $USERNAME
+
+sudo mkdir -p /home/$USERNAME/{projects,logs,backups,docs}
+sudo chown -R $USERNAME:$DEPT /home/$USERNAME
+
+sudo chage -d 0 $USERNAME
+"echo "${date} - Onboard $USERNAME into $DEPT" | sudo tee -a $LOG
+echo "User created succesfully!"
+echo "Temporary password: $REMP_PASS"
