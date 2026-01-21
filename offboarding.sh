f544dcdc10195de9cfb3aa16c615ba345cbfce81
Krisney Manuel <krisney83@gmail.com>
2026-01-21 16:56:29 -0500

    Create offboarding script for user management
    Added a script to offboard users by disabling accounts and backing up home directories.

--- offboarding.sh
+++ offboarding.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+LOG="/var/log/user_admin.log"
+echo "Enter username to offboard:"
+read USERNAME
 
+sudo usermod -L $USERNAME
+sudo chage -E 0 $USERNAME
+
+sudo tar -czf /home/${USERNAME}_backup_$(date +%F) .tar.gz /home/$USERNAME
+echo "$(date) - offboard $USERNAME and archived home directory" | sudo tee -a $LOG
+echo "User disabled and data backed up."
